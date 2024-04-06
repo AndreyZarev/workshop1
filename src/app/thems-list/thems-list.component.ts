829bb4d17239a2a4ddcfdbd9d2e17ade2f8c11b7
tocode <andrey.zarev@outlook.com>
2024-04-06 18:44:20 +0300

    Create thems-list.component.ts

---
+++ src/app/thems-list/thems-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thems-list',
+  templateUrl: './thems-list.component.html',
+  styleUrls: ['./thems-list.component.css']
+})
+export class ThemsListComponent {
+
+}
